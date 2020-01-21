f4703b9e618e3b39fcbf93163f3ae06bdd513770
Stephen Rosen <sirosen@globus.org>
2020-01-21 18:46:08 +0000

    Add show-ansi-colors func
    Handy for scripting: show a visual reminder of what various ANSI color
    codes do.

--- zsh/.zsh_config/05-general_funcs.zsh
+++ zsh/.zsh_config/05-general_funcs.zsh
@@ -18,6 +18,28 @@ cwd-fileserver () {
   python3 -m http.server "$port" --bind localhost
 }
 
+show-ansi-colors () {
+  echo -e '\\e[39m...\\e[0m: '"\e[39mDefault\e[0m"
+  echo -e '\\e[30m...\\e[0m: '"\e[30mBlack\e[0m"
+  echo -e '\\e[31m...\\e[0m: '"\e[31mRed\e[0m"
+  echo -e '\\e[32m...\\e[0m: '"\e[32mGreen\e[0m"
+  echo -e '\\e[33m...\\e[0m: '"\e[33mYellow\e[0m"
+  echo -e '\\e[34m...\\e[0m: '"\e[34mBlue\e[0m"
+  echo -e '\\e[35m...\\e[0m: '"\e[35mMagenta\e[0m"
+  echo -e '\\e[36m...\\e[0m: '"\e[36mCyan\e[0m"
+  echo -e '\\e[37m...\\e[0m: '"\e[37mLight gray\e[0m"
+  echo -e '\\e[90m...\\e[0m: '"\e[90mDark gray\e[0m"
+  echo -e '\\e[91m...\\e[0m: '"\e[91mLight red\e[0m"
+  echo -e '\\e[92m...\\e[0m: '"\e[92mLight green\e[0m"
+  echo -e '\\e[93m...\\e[0m: '"\e[93mLight yellow\e[0m"
+  echo -e '\\e[94m...\\e[0m: '"\e[94mLight blue\e[0m"
+  echo -e '\\e[95m...\\e[0m: '"\e[95mLight magenta\e[0m"
+  echo -e '\\e[96m...\\e[0m: '"\e[96mLight cyan\e[0m"
+  echo -e '\\e[97m...\\e[0m: '"\e[97mWhite\e[0m"
+  echo -e '\\e[1;31m...\\e[0m: '"\e[1;31mBold Red\e[0m"
+  echo -e '\\e[1;33m...\\e[0m: '"\e[1;33mBold Yellow\e[0m"
+}
+
 magick-hostname-background-gnome3 () {
     local image_loc="$HOME/Pictures/autogenbg.png"
     convert -size 1440x900 xc:lightgrey -font Palatino-Bold -pointsize 128 -fill black -draw "text 400,300 '$(hostname)'" -blur 0x1 "$image_loc"
