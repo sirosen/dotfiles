3d7d63543e79f551ecb26fd0c50b2cd4e56793a2
Stephen Rosen <sirosen@globus.org>
2024-11-26 08:33:43 -0600

    Remove stale completer

--- zsh/.zsh_config/07-completions.zsh
+++ zsh/.zsh_config/07-completions.zsh
@@ -3,11 +3,6 @@ if type globus > /dev/null; then
     eval "$(globus --zsh-completer)"
 fi
 
-# globus-automate completer
-if type globus-automate > /dev/null; then
-    eval "$(globus-automate --show-completion)"
-fi
-
 # ec2ssh completer
 if type ec2ssh > /dev/null; then
     eval "$(_ec2ssh_print_completion --shell zsh)"
